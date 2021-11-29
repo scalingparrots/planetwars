639217e285115e5641f8903a524e7274dafc4cd9
dprimo <daniele.primasso@scalingparrots.com>
2021-11-29 13:52:34 +0100

    first deploy

--- test/main.ts
+++ test/main.ts
@@ -1,20 +1,19 @@
-import hardhat from "hardhat";
+import hardhat, {upgrades} from "hardhat";
 import {expect} from 'chai';
 import { Contract } from "@ethersproject/contracts";
 import {BigNumber} from "ethers";
 const { ethers } = hardhat;
 let catchRevert = require("./utils/exceptions.ts").catchRevert;
-let catchRevertWithoutReason = require("./utils/exceptions.ts").catchRevertWithoutReason;
 let GoldContract : Contract;
 
 let DEFAULT_ADMIN_ROLE = ethers.utils.keccak256("0x00");
 let MINTER_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('MINTER_ROLE'));
-let BURN_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('BURNER_ROLE'));
+let BURNER_ROLE = ethers.utils.keccak256(ethers.utils.toUtf8Bytes('BURNER_ROLE'));
 
 let decimalsBigNumber = BigNumber.from(10000000000).mul(100000000)
 
-let initialSupply:BigNumber = BigNumber.from(25000000).mul(decimalsBigNumber);
-let token1:BigNumber = BigNumber.from(100).mul(decimalsBigNumber);
+let initialSupply:BigNumber = BigNumber.from(10000000).mul(decimalsBigNumber);
+let tokenAmount1:BigNumber = BigNumber.from(100).mul(decimalsBigNumber);
 
 let deployer: any
 let account1: any
@@ -25,92 +24,91 @@ let account5: any
 
 before(async function() {
   [deployer, account1, account2, account3, account4, account5] = await ethers.getSigners()
+
+  const goldContractFactory = await ethers.getContractFactory("Gold");
+  GoldContract = await upgrades.deployProxy(goldContractFactory, []);
+  await GoldContract.deployed();
+
+  let roleAdmin = await GoldContract.getRoleAdmin(DEFAULT_ADMIN_ROLE)
+  expect(await GoldContract.hasRole(roleAdmin, deployer.address)).to.be.true
 });
 
 describe("Testing... ", async () => {
 
-  describe("Testing deploy", async () => {
+  describe("Testing ERC20 standard", async () => {
 
-    it("should deploy gold contract", async () => {
-      const goldContractFactory = await ethers.getContractFactory("Gold");
-      GoldContract = await upgrades.deployProxy(goldContractFactory, []);
-      await GoldContract.deployed();
-
+    it("should test grant role minter", async () => {
       await GoldContract.grantRole(MINTER_ROLE, deployer.address);
-      await GoldContract.mint(deployer.address, initialSupply);
-      await GoldContract.revokeRole(MINTER_ROLE, deployer.address);
-
-      let roleAdmin = await GoldContract.getRoleAdmin(DEFAULT_ADMIN_ROLE)
-      let isAdmin = await GoldContract.hasRole(roleAdmin, deployer.address)
-      expect(isAdmin).to.be.true
+      await catchRevert(GoldContract.connect(account2).grantRole(MINTER_ROLE, account3.address));
+      expect(await GoldContract.hasRole(MINTER_ROLE, deployer.address)).to.be.true
     })
 
-    it('should try mint token1 with admin (not minter) to account 2', async () => {
-      await catchRevert(GoldContract.mint(account2.address, token1))
+    it("should test mint", async () => {
+      await GoldContract.mint(deployer.address, initialSupply);
+      await catchRevert(GoldContract.connect(account2).mint(deployer.address, initialSupply));
+      expect(await GoldContract.totalSupply()).to.be.equal(initialSupply);
+      expect(await GoldContract.balanceOf(deployer.address)).to.be.equal(initialSupply);
+    })
 
-      let balanceOfAccount2 = await GoldContract.balanceOf(account2.address);
-      expect(balanceOfAccount2).to.be.equal(0);
+    it('should test transfer', async () => {
+      await GoldContract.transfer(account1.address, tokenAmount1);
+      await catchRevert(GoldContract.transfer(account1.address, initialSupply.add(tokenAmount1)));
+      expect(await GoldContract.balanceOf(deployer.address)).to.be.equal(initialSupply.sub(tokenAmount1))
+      expect(await GoldContract.balanceOf(account1.address)).to.be.equal(tokenAmount1)
     });
 
-    it('should add account 1 as minter', async () => {
-      await GoldContract.grantRole(MINTER_ROLE, account1.address);
+    it('should test approve and transfer from', async () => {
+      let balanceDeployer = await GoldContract.balanceOf(deployer.address)
+      let balanceAccount = await GoldContract.balanceOf(account3.address)
 
-      let isMinter = await GoldContract.hasRole(MINTER_ROLE, account1.address)
-      expect(isMinter).to.be.true
+      await GoldContract.approve(account2.address, tokenAmount1);
+      expect(await GoldContract.allowance(deployer.address, account2.address)).to.be.equal(tokenAmount1);
+
+      await GoldContract.connect(account2).transferFrom(deployer.address, account3.address, tokenAmount1);
+      await catchRevert(GoldContract.connect(account3).transferFrom(deployer.address, account3.address, tokenAmount1));
+
+      expect(await GoldContract.balanceOf(account3.address)).to.be.equal(balanceAccount.add(tokenAmount1));
+      expect(await GoldContract.balanceOf(deployer.address)).to.be.equal(balanceDeployer.sub(tokenAmount1));
     });
 
-    it('should try mint token1 with admin (not minter) to account 2', async () => {
-      await catchRevert(GoldContract.mint(account2.address, token1))
-      let balanceOfAccount2 = await GoldContract.balanceOf(account2.address);
-      expect(balanceOfAccount2).to.be.equal(0);
+    it('should test revoke role', async () => {
+      await catchRevert(GoldContract.connect(account2).revokeRole(MINTER_ROLE, deployer.address));
+      await GoldContract.revokeRole(MINTER_ROLE, deployer.address);
+      expect(await GoldContract.hasRole(MINTER_ROLE, deployer.address)).to.be.false
     });
 
-    it('should mint token1 with account1 (minter) to account 2', async () => {
-      await GoldContract.connect(account1).mint(account2.address, token1)
-      let balanceOfAccount2 = await GoldContract.balanceOf(account2.address);
-      expect(balanceOfAccount2).to.be.equal(token1);
-    });
+    it("should test grant role burner", async () => {
+      await GoldContract.grantRole(BURNER_ROLE, account2.address);
+      await catchRevert(GoldContract.connect(account1).grantRole(BURNER_ROLE, account2.address));
+      expect(await GoldContract.hasRole(BURNER_ROLE, account2.address)).to.be.true
+    })
 
-    it('should revoke role to account 1', async () => {
-      await GoldContract.revokeRole(MINTER_ROLE, account1.address)
-
-      let isMinter = await GoldContract.hasRole(MINTER_ROLE, account1.address)
-      expect(isMinter).to.be.false
-    });
-
-    it('should try mint token1 with accout1 (not minter already) to account 2', async () => {
-      await catchRevert(GoldContract.connect(account1).mint(account2.address, token1))
-      let balanceOfAccount2 = await GoldContract.balanceOf(account2.address);
-      expect(balanceOfAccount2).to.be.equal(token1);
-    });
-
-    it('should try burn token1 with account 2 (no burner )', async () => {
-      await catchRevert(GoldContract.connect(account2).burn(account2.address, token1));
-
+    it("should test burn", async () => {
+      let balanceDeployer = await GoldContract.balanceOf(deployer.address)
       let totalSupply = await GoldContract.totalSupply();
-      expect(totalSupply).to.be.equal(initialSupply.add(token1))
+      await GoldContract.connect(account2).burn(deployer.address, tokenAmount1);
+      await catchRevert(GoldContract.connect(account3).burn(deployer.address, tokenAmount1));
+      expect(await GoldContract.totalSupply()).to.be.equal(totalSupply.sub(tokenAmount1));
+      expect(await GoldContract.balanceOf(deployer.address)).to.be.equal(balanceDeployer.sub(tokenAmount1))
+    })
+
+    it('should test transfer from with burn', async () => {
+      let balanceDeployer = await GoldContract.balanceOf(deployer.address)
+      let balanceAccount = await GoldContract.balanceOf(account5.address)
+      await GoldContract.approve(account2.address, tokenAmount1);
+      await GoldContract.connect(account2).transferFromWithBurn(deployer.address, account5.address, tokenAmount1);
+      await catchRevert(GoldContract.connect(account3).transferFromWithBurn(deployer.address, account5.address, tokenAmount1));
+      expect(await GoldContract.balanceOf(account5.address)).to.be.equal(balanceAccount.add(tokenAmount1));
+      expect(await GoldContract.balanceOf(deployer.address)).to.be.equal(balanceDeployer.sub(tokenAmount1));
     });
 
-    it('should add account 2 as burner', async () => {
-      await GoldContract.grantRole(BURN_ROLE, account2.address);
-
-      let isBurner = await GoldContract.hasRole(BURN_ROLE, account2.address)
-      expect(isBurner).to.be.true
-    });
-
-    it('should burn token1 with account 2 (burner )', async () => {
-      await GoldContract.connect(account2).burn(account2.address, token1);
-
-      let totalSupply = await GoldContract.totalSupply();
-      expect(totalSupply).to.be.equal(initialSupply)
-    });
-
-    it('should renounce admin role', async () => {
+    it('should test revoke role admin', async () => {
       let roleAdmin = await GoldContract.getRoleAdmin(DEFAULT_ADMIN_ROLE)
-      await GoldContract.renounceRole(roleAdmin, deployer.address)
 
-      let isAdmin = await GoldContract.hasRole(DEFAULT_ADMIN_ROLE, deployer.address)
-      expect(isAdmin).to.be.false
+      //await GoldContract.revokeRole(roleAdmin, deployer.address);
+      await GoldContract.renounceRole(roleAdmin, deployer.address);
+      expect(await GoldContract.hasRole(roleAdmin, deployer.address)).to.be.false
     });
   })
+
 });
